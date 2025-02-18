033f43efd62b78fefc1fb65b8ad95a1d3c468584
minh <22024547@vnu.edu.vn>
2025-02-18 17:37:15 +0700

    Initial commit

---
+++ src/main/java/com/example/testing_001/Testing001Application.java
@@ -0,0 +1,14 @@
+package com.example.testing_001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+
+public class Testing001Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Testing001Application.class, args);
+	}
+}
+
